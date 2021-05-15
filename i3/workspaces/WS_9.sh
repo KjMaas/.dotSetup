73260eb097f760b15f76f6bc990b6da456fc0079
Klaasjan Maas <klaasjan.maas@laposte.net>
2021-05-16 01:51:46 +0200

    modified blender WS

--- i3/workspaces/WS_9.sh
+++ i3/workspaces/WS_9.sh
@@ -6,5 +6,8 @@ i3-msg 'workspace 9'
 i3-msg 'append_layout ~/.dotSetup/i3/workspaces/WS_9.json'
 
 # load applications
-blender &
+# blender &
+
+export PATH=/home/kjm/.pyenv/versions/3.8.6/envs/blenderLauncher/bin/:$PATH
+python ~/Documents/Blender/Blender-Launcher/source/main.py &
 
